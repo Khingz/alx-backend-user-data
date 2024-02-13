b65bcc408879165f48c29b621788e9c946eb43c9
khingz <kingsley995mr@gmail.com>
2024-02-13 20:41:35 +0000

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,19 @@ from .auth import Auth
 class BasicAuth(Auth):
     """BasicAuth class
     """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """Comment
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        auth_head = authorization_header.split()
+        if auth_head[1] and len(auth_head[1]) > 0:
+            return auth_head[1]
+        return None
