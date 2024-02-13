e401731a75c54d6f51599fabd278dcc7473c2952
khingz <kingsley995mr@gmail.com>
2024-02-13 20:18:09 +0000

    check failing

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ auth_type = os.environ.get('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-if auth_type == 'BasicAuth()':
+if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
