e795a3fe68112461b5118284c12d63d56a7c74c0
khingz <kingsley995mr@gmail.com>
2024-02-15 11:31:07 +0000

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from .auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -20,9 +21,21 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """session id
+        """returns user_id from session id
         """
         if session_id is None or type(session_id) != str:
             return None
         session = self.user_id_by_session_id.get(session_id, None)
         return session
+
+    def current_user(self, request=None):
+        """returns instance of current user
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        user = User.get(user_id)
+        return user
