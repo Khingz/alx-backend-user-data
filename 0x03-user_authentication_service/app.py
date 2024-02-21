30d65edf1c01705f5aace5c881bb11b53655a55e
khingz <kingsley995mr@gmail.com>
2024-02-21 16:01:32 +0000

    task failing

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,17 +63,17 @@ def logout():
 
 
 @app.route('/profile', methods=['GET'])
-def profile() -> str:
-    """ Fetch user profile
+def profile():
+    """ Comment
     """
-    session_id = request.cookies.get('session_id', None)
+    session_id = request.cookies.get("session_id", None)
     if session_id is None:
         abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
-    data = {'email': user.email}
-    return jsonify(data), 200
+    msg = {"email": user.email}
+    return jsonify(msg), 200
 
 
 if __name__ == "__main__":
