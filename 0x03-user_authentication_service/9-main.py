b3d83a906eb6cb8ac0e43d3342f9f2537aa66120
khingz <kingsley995mr@gmail.com>
2024-02-21 16:52:07 +0000

    check failing

--- 0x03-user_authentication_service/9-main.py
+++ 0x03-user_authentication_service/9-main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
-from auth import _generate_uuid()
+from auth import Auth
 
-print(_generate_uuid())
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
+
+auth.register_user(email, password)
+print(auth.get_reset_password_token(email))
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,6 +92,6 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError
-        reset_token = _geneate_uuid()
+        reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
